3a46936258759ad4ed6df80acb856fa0d35cf5da
Semir Ibrahim <semir@teamnetsol.com>
2016-06-20 17:40:24 +0100

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,18 @@
+name := "play-anorm"
+
+version := "0.0.1-SNAPSHOT"
+
+scalaVersion := "2.11.8"
+
+resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
+
+libraryDependencies ++= Seq(
+  jdbc,
+  evolutions,
+  "com.adrianhurt" %% "play-bootstrap" % "1.0-P25-B3",
+  "com.typesafe.play" %% "anorm" % "2.5.0",
+  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % "test",
+  "mysql" % "mysql-connector-java" % "5.1.18"
+)
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,50 @@
+# --- First database schema
+
+# --- !Ups
+
+# -- set ignorecase true;
+
+use `play-anorm`;
+
+DROP TABLE if exists company;
+DROP TABLE if exists computer;
+
+
+create table company (
+  id                        bigint not null,
+  name                      varchar(255) not null,
+  constraint pk_company primary key (id))
+;
+
+create table computer (
+  id                        bigint not null ,
+  name                      varchar(255) not null,
+  introduced                datetime,
+  discontinued              datetime,
+  company_id                bigint,
+  constraint pk_computer primary key (id))
+;
+
+create sequence company_seq start with 1000;
+
+create sequence computer_seq start with 1000;
+
+alter table computer add constraint fk_computer_company_1 foreign key (company_id) references company (id) on delete restrict on update restrict;
+create index ix_computer_company_1 on computer (company_id);
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists company;
+
+drop table if exists computer;
+
+# -- SET REFERENTIAL_INTEGRITY TRUE;
+SET FOREIGN_KEY_CHECKS TRUE;
+
+drop sequence if exists company_seq;
+
+drop sequence if exists computer_seq;
+
